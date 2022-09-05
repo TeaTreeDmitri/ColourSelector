3a0550237b668c27f4008f38ad4e771b560910a9
Gareth Davies <mrgarethddavies@gmail.com>
2022-09-05 17:05:12 +0200

    3 down 3 out

--- colorconvert.js
+++ colorconvert.js
@@ -7,17 +7,18 @@
 
 cssToRGB("rgb(192, 13, 1)")
 
-function cssToRGB(rgbString) {
+function cssToRGB(cssString) {
     let r;
     let g;
     let b;
-    rgbString = rgbString.substring(rgbString.indexOf(`(`)+1, rgbString.indexOf(")"));
-    rgbString = rgbString.replace(" ","");
-    rgbString = rgbString.split(",");
-    r = parseInt(rgbString[0]);
-    g = parseInt(rgbString[1]);
-    b = parseInt(rgbString[2]);
-    console.log(rgbString, r, g, b);
+    cssString = cssString.substring(cssString.indexOf(`(`) + 1, cssString.indexOf(")"));
+    cssString = cssString.replace(" ", "");
+    cssString = cssString.split(",");
+
+    r = parseInt(cssString[0]);
+    g = parseInt(cssString[1]);
+    b = parseInt(cssString[2]);
+    console.log("css > rgb values: ", r, g, b);
 }
 
 //HEX TO RBG///////////////////////////////////////////////////////////////
@@ -37,11 +38,11 @@ function hexToRGB(hexString) {
     let g;
     let b;
     let rgbString
-    hexString = hexString.substring(hexString.indexOf("#")+1);
+    hexString = hexString.substring(hexString.indexOf("#") + 1);
     r = parseInt(hexString.substring(0, 2), 16);
     g = parseInt(hexString.substring(2, 4), 16);
     b = parseInt(hexString.substring(4, 6), 16);
-    console.log(r, g, b);
+    console.log("hexcode > rgb values: ", r, g, b);
 
     //BELOW FOR RBG TO CSS
     // r = r.trim();
@@ -53,3 +54,30 @@ function hexToRGB(hexString) {
 
 //RGB TO HEX//////////////////////////////////////////////////////////////
 
+// r	g	b	hex
+// 186	218	85	#bada55
+// 250	202	222	#facade
+// 192	255	238	#c0ffee
+// 240	11	66	#f00b42
+// 192	222	37	#c0de25
+let rgbString = {
+    r: 192,
+    g: 222,
+    b: 37,
+}
+
+rbgToHex(rgbString);
+
+function rbgToHex(rgbString) {
+    let r = rgbString.r;
+    let g = rgbString.g;
+    let b = rgbString.b;
+
+    r = r.toString(16).padStart(2, "0");
+    g = g.toString(16).padStart(2, "0");
+    b = b.toString(16).padStart(2, "0");
+    let hexString = "#" + r + g + b;
+
+
+    console.log("rgb > hexcode: ", hexString)
+}
